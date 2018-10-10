c60b947e6955f63a792bbc6c94dbae006d291642
zhygmsz <652246564@qq.com>
2018-10-11 01:47:37 +0800

    开始搭建UI框架

---
+++ Client-zhy/Assets/Code/Scripts/Mgr/UIMgr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr
+{
+    static private Dictionary<int, UIFrame> mUIDic = new Dictionary<int, UIFrame>();
+
+	static public void OnClick(int uiid)
+	{
+
+	}
+
+	static public void OnPress(int uiid, bool isPressed)
+	{
+
+	}
+
+	static public void OnDrag(int uiid, Vector2 delta)
+	{
+		
+	}
+}
---
+++ Client-zhy/Assets/Code/Scripts/UI/UIEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvent : MonoBehaviour
+{
+    public int mUIID;
+    public Collider mCollider;
+
+	void Awake()
+	{
+        mCollider = GetComponent<BoxCollider>();
+    }
+
+	void OnClick()
+	{
+		
+	}
+
+	void OnPress(bool isPressed)
+	{
+
+	}
+
+	void OnDrag(Vector2 delta)
+	{
+		
+	}
+}
