8a4fcc2d618bae398a4af3c1ce6b7cb9c5e95af6
zhygmsz <652246564@qq.com>
2018-10-14 00:09:50 +0800

    完善UIMgr和UIFrame，列提纲

--- Client-zhy/Assets/Code/Scripts/Mgr/UIMgr.cs
+++ Client-zhy/Assets/Code/Scripts/Mgr/UIMgr.cs
@@ -6,6 +6,44 @@ public class UIMgr
 {
     static private Dictionary<int, UIFrame> mUIDic = new Dictionary<int, UIFrame>();
 
+	//下面两个方法调度UIFrame
+	//UI关联的资源处理细节放到UIFrame里做
+	static public void ShowUI()
+	{
+		//打开UI时，如果不存在
+	}
+
+	static public void UnShowUI()
+	{
+
+	}
+
+	//下面的方法是，UI的状态回调，由UIFrame发起
+	//在这里做转发，传递给lua层
+	//lua层再根据uiid派发给各个UI对应的lua脚本
+	static public void OnCreate()
+	{
+
+	}
+
+	static public void OnEnable()
+	{
+
+	}
+
+	static public void OnDisable()
+	{
+
+	}
+
+	static public void OnDestroy()
+	{
+
+	}
+
+	//下面的方法是各种UI事件方法，由UIEvent调用，直接传递到lua层
+	//在UIMgr里接受lua注册进来的各种UI事件的方法，不同的界面以uiid来区分
+	//在lua层根据uiid再做一次派发，直到每个UI事件传递到UI对应的lua脚本里
 	static public void OnClick(int uiid)
 	{
 
--- Client-zhy/Assets/Code/Scripts/UI/UIFrame.cs
+++ Client-zhy/Assets/Code/Scripts/UI/UIFrame.cs
@@ -2,7 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//非继承自MonoBehaviour
 public class UIFrame
 {
-	
+	//在创建UI时，获取UIEvent脚本数组，把UIEvent.UIID设置成该UIFrame对应的
+    //这样，在UIEvent里可以根据uiid去掉用UIMgr的事件方法
+
+    //在UIFrame里处理UI关联的资源细节，以及和资源管理的对接
+
+    //UI的预处理和后处理，都需要在这里做
+
+    //对外保证四个大状态
+
 }
