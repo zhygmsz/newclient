69897b4d6a918dbebb8d11b7a3385f9af2b96520
zhaohaoyang <652246564@qq.com>
2018-10-16 00:07:19 +0800

    测试UI lua脚本

---
+++ Client-zhy/Assets/Lua/UI/Test/UI_Test.lua
@@ -0,0 +1,53 @@
+module("UI_Test", package.seeall)
+
+function OnCreate(self)
+    GameLog.Log("---------------------OnCreate")
+end
+
+function OnEnable(self)
+    GameLog.Log("---------------------OnEnable")
+end
+
+function OnDisable(self)
+    GameLog.Log("---------------------OnDisable")
+end
+
+function OnDestroy(self)
+    GameLog.Log("---------------------OnDestroy")
+end
+
+function OnClick(id)
+    
+end
+
+function OnPress(isPressed, id)
+    
+end
+
+function OnSelect(selected, id)
+    
+end
+
+function OnDoubleClick(id)
+
+end
+
+function OnDragStart(id)
+    
+end
+
+function OnDrag(delta, id)
+    
+end
+
+function OnDragEnd(id)
+    
+end
+
+function OnDragOver(obj, id)
+    
+end
+
+function OnDragOut(obj, id)
+    
+end
