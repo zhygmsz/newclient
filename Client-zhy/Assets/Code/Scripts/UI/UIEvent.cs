0cb793e5428277abf42f5f66bc4a7c8558cd4035
zhaohaoyang <652246564@qq.com>
2018-10-15 23:59:49 +0800

    继续完善UI框架

--- Client-zhy/Assets/Code/Scripts/UI/UIEvent.cs
+++ Client-zhy/Assets/Code/Scripts/UI/UIEvent.cs
@@ -2,75 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIEvent : MonoBehaviour
+namespace GameCore
 {
-    //由面板赋值
-    public int mEventID;
+    public class UIEvent : MonoBehaviour
+    {
+        //由面板赋值
+        public int mEventID;
 
-    //由UIFrame创建时，赋值
-    private int mUIID;
+        //由UIFrame创建时，赋值
+        private int mUIID;
 
-    private Collider mCollider;    
+        private Collider mCollider;
 
-	public int UIID
-	{
-		get
-		{
-            return mUIID;
+        public int UIID
+        {
+            get
+            {
+                return mUIID;
+            }
+            set
+            {
+                mUIID = value;
+            }
         }
-		set
-		{
-            mUIID = value;
+
+        void Awake()
+        {
+            mCollider = GetComponent<BoxCollider>();
         }
-	}
 
-	void Awake()
-	{
-        mCollider = GetComponent<BoxCollider>();
+        void OnClick()
+        {
+            UIMgr.OnClick(UIID, mEventID);
+        }
+
+        void OnPress(bool isPressed)
+        {
+            UIMgr.OnPress(UIID, isPressed, mEventID);
+        }
+
+        void OnSelect(bool selected)
+        {
+            UIMgr.OnSelect(UIID, selected, mEventID);
+        }
+
+        void OnDoubleClick()
+        {
+            UIMgr.OnDoubleClick(UIID, mEventID);
+        }
+
+        void OnDragStart()
+        {
+            UIMgr.OnDragStart(UIID, mEventID);
+        }
+
+        void OnDrag(Vector2 delta)
+        {
+            UIMgr.OnDrag(UIID, delta, mEventID);
+        }
+
+        void OnDragEnd()
+        {
+            UIMgr.OnDragEnd(UIID, mEventID);
+        }
+
+        void OnDragOver(GameObject obj)
+        {
+            UIMgr.OnDragOver(UIID, obj, mEventID);
+        }
+
+        void OnDragOut(GameObject obj)
+        {
+            UIMgr.OnDragOut(UIID, obj, mEventID);
+        }
     }
 
-	void OnClick()
-	{
-        UIMgr.OnClick(UIID, mEventID);
-	}
-
-	void OnPress(bool isPressed)
-	{
-        UIMgr.OnPress(UIID, isPressed, mEventID);
-	}
-
-    void OnSelect(bool selected)
-    {
-        UIMgr.OnSelect(UIID, selected, mEventID);
-    }
-
-    void OnDoubleClick()
-    {
-        UIMgr.OnDoubleClick(UIID, mEventID);
-    }
-
-    void OnDragStart()
-    {
-        UIMgr.OnDragStart(UIID, mEventID);
-    }
-
-	void OnDrag(Vector2 delta)
-	{
-        UIMgr.OnDrag(UIID, delta, mEventID);
-	}
-
-    void OnDragEnd()
-    {
-        UIMgr.OnDragEnd(UIID, mEventID);
-    }
-
-    void OnDragOver(GameObject obj)
-    {
-        UIMgr.OnDragOver(UIID, obj, mEventID);
-    }
-
-    void OnDragOut(GameObject obj)
-    {
-        UIMgr.OnDragOut(UIID, obj, mEventID);
-    }
 }
