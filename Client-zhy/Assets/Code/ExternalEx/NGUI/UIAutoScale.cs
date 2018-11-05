b6adf77f8f78c7bfebbba5d818edaddd7f1616d4
zhaohaoyang <652246564@qq.com>
2018-11-05 23:35:03 +0800

    提交聊天UI

---
+++ Client-zhy/Assets/Code/ExternalEx/NGUI/UIAutoScale.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UIAutoScale : MonoBehaviour
+{
+    public UIRoot mRoot;
+    public Transform mTransform;
+
+    private void Start()
+    {
+        mTransform = transform;
+        mRoot = NGUITools.FindInParents<UIRoot>(mTransform);
+    }
+
+    private void Update()
+    {
+        if(mTransform != null && mRoot != null)
+        {
+            UpdateScale();
+        }
+    }
+
+    private void UpdateScale()
+    {
+        int activeHeight = mRoot.activeHeight;
+        float size = activeHeight != mRoot.manualHeight ? (float)activeHeight / mRoot.manualHeight : 1f;
+        Vector3 ls = mTransform.localScale;
+        if(!(Mathf.Abs(ls.x - size) <= float.Epsilon) || !(Mathf.Abs(ls.y - size) <= float.Epsilon) || !(Mathf.Abs(ls.z - size) <= float.Epsilon))
+        {
+            mTransform.localScale = new Vector3(size, size, size);
+        }
+    }
+}
