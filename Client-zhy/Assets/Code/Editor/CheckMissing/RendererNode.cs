d85f7e75fa43b0fd64c5bac50f7ea9f825465a1c
zhaohaoyang <652246564@qq.com>
2018-11-25 23:46:52 +0800

    添加材质节点

---
+++ Client-zhy/Assets/Code/Editor/CheckMissing/RendererNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YamlParser
+{
+    public class RendererNode : NodeBase
+    {
+        protected string mGameObjectFileID;
+
+
+        public string GameObjectFileID
+        {
+            get { return mGameObjectFileID; }
+        }
+
+
+        public RendererNode()
+        {
+
+        }
+
+        public override void Begin()
+        {
+            base.Begin();
+        }
+
+        public override void End()
+        {
+            base.End();
+        }
+
+        public override void Append(string line)
+        {
+            base.Append(line);
+            if (line.Contains("m_GameObject"))
+            {
+                mGameObjectFileID = GetOneFileID(line);
+            }
+        }
+    }
+}
