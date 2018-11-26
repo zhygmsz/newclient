a466f691a58b2b60229feb3792c854cc8cc2b114
zhaohaoyang <652246564@qq.com>
2018-11-26 23:04:03 +0800

    测试prefab

---
+++ Client-zhy/Assets/Code/Logic/monoscript1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monoscript1 : MonoBehaviour
+{
+    public UITexture mTex1;
+    public UITexture mTex2;
+    public Texture2D mTex2D1;
+    public Texture2D mTex2D2;
+    public UIAtlas mUIAatlas1;
+    public UIAtlas mUIAatlas2;
+    public UISprite mUISprite1;
+    public UISprite mUISprite2;
+    public Material mMat1;
+    public Material mMat2;
+    public ParticleSystem mPs1;
+    public ParticleSystem mPs2;
+}
