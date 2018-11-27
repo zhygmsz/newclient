7f95c74c911efb0c4ec9627717d85728943daff8
zhygmsz <652246564@qq.com>
2018-11-28 02:08:49 +0800

    调整节点划分逻辑，不再用继承层次，改用独立类型

--- Client-zhy/Assets/Code/Editor/CheckMissing/ParserBase.cs
+++ Client-zhy/Assets/Code/Editor/CheckMissing/ParserBase.cs
@@ -10,10 +10,12 @@ namespace YamlParser
         protected string mPath;
 
         protected Dictionary<string, PrefabNode> mPrefabNodeDic = new Dictionary<string, PrefabNode>();
-        protected Dictionary<string, GameObjectNode> mGameObjectNodeDic = new Dictionary<string, GameObjectNode>();
-        protected Dictionary<string, MonoBehaviourNode> mMonoBehaviourNodeDic = new Dictionary<string, MonoBehaviourNode>();
+        protected Dictionary<string, GoNode> mGoNodeDic = new Dictionary<string, GoNode>();
+        protected Dictionary<string, CommonNode> mScriptNodeDic = new Dictionary<string, CommonNode>();
+        protected Dictionary<string, CommonNode> mMatNodeDic = new Dictionary<string, CommonNode>();
+        protected Dictionary<string, CommonNode> mShaderNodeDic = new Dictionary<string, CommonNode>();
 
-        
+
         public ParserBase(string path)
         {
             mPath = path;
@@ -22,12 +24,12 @@ namespace YamlParser
 
         public void LogScriptMissing(HashSet<string> guidSet)
         {
-            foreach (KeyValuePair<string, MonoBehaviourNode> kvp in mMonoBehaviourNodeDic)
+            foreach (KeyValuePair<string, CommonNode> kvp in mScriptNodeDic)
             {
-                if (!guidSet.Contains(kvp.Value.ScriptGUID))
+                if (!guidSet.Contains(kvp.Value.GUID))
                 {
-                    string goName = mGameObjectNodeDic[kvp.Value.GameObjectFileID].Name;
-                    string log = string.Format("path = {0}, guid = {1}, goName = {2}", mPath, kvp.Value.ScriptGUID, goName);
+                    string goName = mGoNodeDic[kvp.Value.GoFileID].Name;
+                    string log = string.Format("path = {0}, guid = {1}, goName = {2}", mPath, kvp.Value.GUID, goName);
                     Debug.LogError(log);
                 }
             }
@@ -37,97 +39,44 @@ namespace YamlParser
         {
             FileStream fs = File.OpenRead(mPath);
             StreamReader sr = new StreamReader(fs);
-            NodeBase node = null;
             string line = "";
             string fileID = "";
-            string nodeName = "";
+            string goFileID = "";
             while (!sr.EndOfStream)
             {
                 line = sr.ReadLine();
                 if (line.Contains("---"))
                 {
-                    //上一个node（如果存在）结束并缓存
-                    if (node != null && fileID != "")
-                    {
-                        node.End();
-                        AddNode(fileID, node);
-                    }
-
+                    //计算出fileID
                     fileID = line.Replace(" ", "").Split('&')[1];
-                    line = sr.ReadLine();
-                    nodeName = line.Split(':')[0];
-                    node = GetNode(nodeName);
-                    if (node != null)
+                    string classID = line.Replace(" ", "").Split('!', '&')[2];
+                    if (classID == "1")
                     {
-                        node.Begin();
+                        //GameObject，假定1000行内能找到该GameObject的m_Name字段
+                        //如果GameObject上挂在的Component数量超过1000个，则该循环失效，但这种情况几乎不可能
+                        for (int idx = 1, count = 1000; idx <= count; ++idx)
+                        {
+                            line = sr.ReadLine();
+                            if (line.Contains("m_Name"))
+                            {
+                                string goName = line.Replace(" ", "").Split(':')[1];
+                                GoNode newGoNode = new GoNode(fileID, goName);
+                                mGoNodeDic.Add(fileID, newGoNode);
+                                break;
+                            }
+                        }
                     }
                 }
-                else if (node != null)
+                else if (line.Contains("m_GameObject"))
                 {
-                    node.Append(line);
+                    //计算出m_GameObject对应的fileID
+                    goFileID = line.Replace(" ", "").Replace("}", "").Split(':')[2];
+                }
+                else if (line.Contains("fileID") && line.Contains("guid"))
+                {
+                    //根据不同的fileID判断具体的node类型
                 }
             }
-            //最后一个node
-            if (node != null && fileID != "")
-            {
-                node.End();
-                AddNode(fileID, node);
-            }
-        }
-
-
-        protected NodeBase GetNode(string nodeName)
-        {
-            NodeBase node = null;
-            if (nodeName == "Prefab")
-            {
-                node = new PrefabNode();
-            }
-            else if (nodeName == "GameObject")
-            {
-                node = new GameObjectNode();
-            }
-            else if (nodeName == "MonoBehaviour")
-            {
-                node = new MonoBehaviourNode();
-            }
-            else
-            {
-
-            }
-            return node;
-        }
-
-        protected void AddNode(string fileID, NodeBase node)
-        {
-            node.FileID = fileID;
-            if (node is PrefabNode)
-            {
-                AddPrefabNode(fileID, (PrefabNode)node);
-            }
-            else if (node is GameObjectNode)
-            {
-                AddGameObjectNode(fileID, (GameObjectNode)node);
-            }
-            else if (node is MonoBehaviourNode)
-            {
-                AddMonoBehaviourNode(fileID, (MonoBehaviourNode)node);
-            }
-        }
-
-        private void AddPrefabNode(string fileID, PrefabNode node)
-        {
-            mPrefabNodeDic.Add(fileID, node);
-        }
-
-        private void AddGameObjectNode(string fileID, GameObjectNode node)
-        {
-            mGameObjectNodeDic.Add(fileID, node);
-        }
-
-        private void AddMonoBehaviourNode(string fileID, MonoBehaviourNode node)
-        {
-            mMonoBehaviourNodeDic.Add(fileID, node);
         }
     }
 }
