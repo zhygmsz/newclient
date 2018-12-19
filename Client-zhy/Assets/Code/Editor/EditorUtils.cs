93948a5634439c48fcad9f3dad166f9fa4983b8e
zhygmsz <652246564@qq.com>
2018-12-20 01:22:17 +0800

    列出UIPrefab上的所有UIPanel以及Hierarchy，以后可以扩展为任意指定脚本

--- Client-zhy/Assets/Code/Editor/EditorUtils.cs
+++ Client-zhy/Assets/Code/Editor/EditorUtils.cs
@@ -8,6 +8,30 @@ public class EditorUtils
     [MenuItem("Assets/工具集/列出UIPrefab上的所有UIPanel")]
     public static void LogUIPanelOfUIPrefab()
     {
+        UnityEngine.Object obj = Selection.activeObject;
+        if (obj == null)
+        {
+            return;
+        }
+        GameObject go = obj as GameObject;
+        if (go == null)
+        {
+            return;
+        }
+        Transform trs = go.transform;
+        LogUIPanel(trs, trs.name);
+    }
 
+    private static void LogUIPanel(Transform root, string path)
+    {
+        foreach (Transform trs in root)
+        {
+            UIPanel panel = trs.GetComponent<UIPanel>();
+            if (panel != null)
+            {
+                Debug.LogError(path + "/" + trs.name);
+            }
+            LogUIPanel(trs, path + "/" + trs.name);
+        }
     }
 }
