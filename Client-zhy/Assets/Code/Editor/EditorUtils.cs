71a42d3a6697d270dd387e1f9edb3f75b159f30c
zhaohaoyang <652246564@qq.com>
2018-12-19 23:33:35 +0800

    添加列出一个UIPrefab上的所有UIPanel工具

---
+++ Client-zhy/Assets/Code/Editor/EditorUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorUtils
+{
+    [MenuItem("Assets/工具集/列出UIPrefab上的所有UIPanel")]
+    public static void LogUIPanelOfUIPrefab()
+    {
+
+    }
+}
